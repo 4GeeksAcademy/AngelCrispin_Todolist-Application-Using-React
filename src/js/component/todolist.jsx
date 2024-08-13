b4280625962148e721cc837bdd74e99549a3988e
Angel Crispin <155650252+AngelCrispin@users.noreply.github.com>
2024-08-13 22:54:52 +0000

    First Commit

---
+++ src/js/component/todolist.jsx
@@ -0,0 +1,47 @@
+import React,{useState,useEffect} from 'react';
+
+let cont=0;
+
+export default function ToDoList () {
+    const [inputValue, setInputValue ] = useState('');
+    const [allTodos, setAllTodos ] = useState([]);
+    
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          if(inputValue.trim() !== "") {
+            let todo = [...allTodos];
+            todo.push({id:cont,value:inputValue});
+            cont++;
+            setAllTodos(todo);
+          }
+        }
+      }
+      
+
+    const deleteToDo = (index) => {
+      let todoAux = [];
+      allTodos.map((item)=>{ 
+        if(item.id!=index){
+          todoAux.push(item);
+        }
+      })  
+      setAllTodos(todoAux);
+    }
+
+    return (
+        
+			  <div className="content mx-auto">
+          <input type="text" className="form-control input" onChange={e => setInputValue(e.target.value)} value={inputValue} onKeyDown={handleKeyDown}/>
+          <div>
+              {allTodos.map((x) => (
+                    <div key={x.id} className="justify-content-between d-flex todoComplete">
+                      <div className='todo'>{x.value}</div>
+                      <div className='delete'>
+                      <i key={x.id} className=" fa-solid fa-xmark" onClick={()=>deleteToDo(x.id)}></i>
+                       </div> 
+                    </div>
+              ))}    
+          </div>
+        </div>)
+}
